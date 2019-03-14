e06c5a7b8d54c161cf770175d5a12cdd56af5a82
植木 <ueki@ueki-no-MacBook-Air.local>
2019-03-14 20:42:47 +0900

    new Lib LCA

---
+++ SizeUnionFind.py
@@ -0,0 +1,29 @@
+class UnionFind:
+    """
+    sizeによる実装
+    """
+
+    def __init__(self, N):
+        self.parent = [i for i in range(N)]
+        self.size = [1 for _ in range(N)]
+
+    def find(self, x):
+        if self.parent[x] == x:
+            return x
+        else:
+            return self.find(self.parent[x])
+
+    def union(self, x, y):
+        px = self.find(x)
+        py = self.find(y)
+        if px == py:
+            return
+        if self.size[px] < self.size[py]:
+            self.parent[px] = py
+            self.size[py] += self.size[px]
+        else:
+            self.parent[py] = px
+            self.size[px] += self.size[py]
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
