3b87bd9f7bf2a35e5d216896087f3bfb00872243
Ueeek <ueki.k.ae@m.titech.ac.jp>
2020-04-08 22:08:04 +0900

    lazy seg

--- SuffixArray.py
+++ SuffixArray.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import bisect
 
 
@@ -24,7 +25,94 @@ class SuffixArray:
         set self.suffix array:
             list:(index,suffix)
         """
-        pass
+
+        def build_LS_array():
+            """
+            """
+            N = len(self.string)
+            L = [0]*N
+            L[-1] = "S"
+            for i in reversed(range(N-1)):
+                if self.string[i] > self.string[i+1]:
+                    L[i] = "L"
+                elif self.string[i] < self.string[i+1]:
+                    L[i] = "S"
+                else:
+                    L[i] = L[i+1]
+            return L
+
+        def find_lmss(LS, cheat=False):
+            """
+            return set of LMS
+            """
+            N = len(self.string)
+            LMS = [i for i in range(1, N) if LS[i-1] == "L" and LS[i] == "S"]
+            if cheat:
+                LMS = sorted(LMS, key=lambda x: self.string[x:], reverse=False)
+                print("LMS=>", LMS)
+            return LMS
+
+        def induced_sort(LS, lmss):
+            suffix_array = [None]*N
+            C = Counter(self.string)
+            bin_start = dict()
+            bin_cnt = dict()
+            tmp = 0
+            for key, val in sorted(C.items(), key=lambda x: x[0]):
+                bin_cnt[key] = 0
+                bin_start[key] = tmp
+                tmp += val
+
+            for i in reversed(lmss):
+                c = self.string[i]
+                suffix_array[bin_start[c]+C[c]-bin_cnt[c]-1] = i
+                bin_cnt[c] += 1
+
+            for key, val in bin_cnt.items():
+                bin_cnt[key] = 0
+            for e in suffix_array:
+                if e is None or e == 0 or LS[e-1] == "S":
+                    continue
+                ch = self.string[e-1]
+                suffix_array[bin_start[ch]+bin_cnt[ch]] = e-1
+                bin_cnt[ch] += 1
+
+            for key, val in bin_cnt.items():
+                bin_cnt[key] = 0
+            for e in reversed(suffix_array):
+                if e is None or e == 0 or LS[e-1] == "L":
+                    continue
+                ch = self.string[e-1]
+                suffix_array[bin_start[ch]+C[ch]-bin_cnt[ch]-1] = e-1
+                bin_cnt[ch] += 1
+            return suffix_array
+
+        N = len(self.string)
+        suffix_array = [None]*N
+        LS = build_LS_array()
+        LMS = find_lmss(LS, cheat=True)
+        suffix_array = induced_sort(LS, LMS)
+
+        name = 0
+        prev = -1
+        pLMS = {}
+        for e in suffix_array:
+            if e in LMS:
+                for i in range(N):
+                    if prev == -1 or self.string[e+i] != self.string[prev+i]:
+                        name += 1
+                        prev = e
+                        break
+                    elif i and (e+i in LMS or prev+i in LMS):
+                        break
+                pLMS[e] = name-1
+
+        if name < len(LMS):
+            sublst = [pLMS[e] for e in LMS if e < N-1]
+            print("SUBLST=>", sublst)
+        else:
+            LMS = [e for e in reversed(suffix_array) if e in LMS]
+        return induced_sort(LS, LMS)
 
     def build_suffix_array_manber_mayers(self):
         """
@@ -79,16 +167,22 @@ class SuffixArray:
         query += self.pattern
         l = bisect.bisect_left([v[1] for v in self.suffix_array], query)
         r = bisect.bisect_right([v[1] for v in self.suffix_array], query)
-        match_pos = [list(range(l, r+1))]
+        match_pos = list(range(l, r+1))
+        for i in match_pos:
+            print("match=>", self.string[i:])
         return match_pos
 
 
 if __name__ == "__main__":
     S = "nonsense"
+    S = "mmiissiissiippii"
     SA = SuffixArray(S)
     SA.build_suffix_array_naive()
-    print("naive=>", SA.suffix_array)
-    SA.build_suffix_array_manber_mayers()
-    print("manver_mayers=>", SA.suffix_array)
+    #print("naive=>", SA.suffix_array)
+    # SA.build_suffix_array_manber_mayers()
+    #print("manver_mayers=>", SA.suffix_array)
+    # SA.build_suffix_array_sa_is()
+    #print("sa-is=>", SA.suffix_array)
     ret = SA.search("nse")
+    ret = SA.search("ppii")
     print(ret)
