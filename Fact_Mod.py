9dbeda1e311932d9cc3ee8d97ada90d6aee84ed5
Ueeek <ueki.k.ae@m.titech.ac.jp>
2021-03-30 23:29:39 +0900

    fix  comb

--- Fact_Mod.py
+++ Fact_Mod.py
@@ -3,6 +3,8 @@ class FactMod():
     '''
     modの値が素数の時のfactと組み合わせを求める
     フェルマーの小定理を用いているため、modが素数以外の時は使えない
+
+    NとかRの値が固定なら、combとか、複数回呼び出すより先に、計算してしまってlistとかで持っておく方が良い。(TLE対策)
     '''
 
     def __init__(self, n, mod):
@@ -25,12 +27,16 @@ class FactMod():
         '''
         n!の値を返す
         '''
+        assert n>=0
         return self.f[n]
 
     def comb(self, n, r):
         '''
         nCrの値を返す
         '''
+        #assert n>=r>=0
+        if r==0:
+            return 1
         ret = self.f[n] * self.inv_f[n-r]*self.inv_f[r]
         ret %= self.mod
         return ret
@@ -39,6 +45,7 @@ class FactMod():
         """
         nPrの値を返す
         """
+        assert n>=r>=0
         ret = self.f[n] * self.inv_f[n-r]
         ret %= self.mod
         return ret
@@ -46,22 +53,22 @@ class FactMod():
     def div(self,x,y):
         """
         x/yの値を返す
+        このclassにいるべきじゃないな
         """
         return (x*pow(y,self.mod-2,self.mod))%self.mod
 
-"""
-FactMod(N,MOD)のNの値が大きいと、前計算が重くて、動かない。
-nCrのrが小さいという保証があるなら、以下のコードでも、十分早いはず
-"""
-def comb(n,r,MOD):
-    """
-    nCr=\frac{\Pi_{i=0}^{i=r-1}(n-i)}{\Pi_{i=1}^{i=r}(i)}
-    """
-    ret=1
-    for i in range(r):#分子の計算
-        ret*=(n-i)
-        ret%=MOD
-    for i in range(2,r+1):#分母の計算(powでinv(i)を求める。)
-        ret*=pow(i,MOD-2,MOD)
-        ret%=MOD
-    return ret
+    def comb_low_r(self,n,r,MOD):
+        """
+        nCr=\frac{\Pi_{i=0}^{i=r-1}(n-i)}{\Pi_{i=1}^{i=r}(i)}
+            FactMod(N,MOD)のNの値が大きいと、前計算が重くて、動かない。
+            nCrのrが小さいという保証があるなら、以下のコードでも、十分早いはず
+        """
+        ret=1
+        for i in range(r):#分子の計算
+            ret*=(n-i)
+            ret%=MOD
+        for i in range(2,r+1):#分母の計算(powでinv(i)を求める。)
+            ret*=pow(i,MOD-2,MOD)
+            ret%=MOD
+        return ret
+
